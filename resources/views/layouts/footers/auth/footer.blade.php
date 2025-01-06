3e3a097983c29acb11f97029c5c6b4a7d42c4622
zabuzaff <zabuzaff@gmail.com>
2025-01-07 00:30:55 +0800

    update

--- resources/views/layouts/footers/auth/footer.blade.php
+++ resources/views/layouts/footers/auth/footer.blade.php
@@ -7,6 +7,7 @@
                     <script>
                         document.write(new Date().getFullYear())
                     </script>
+                    , Powered by LaraBuild
                     {{-- made with <i class="fa fa-heart"></i> by
                     <a href="https://www.creative-tim.com" class="font-weight-bold" target="_blank">Creative Tim</a>
                     &
